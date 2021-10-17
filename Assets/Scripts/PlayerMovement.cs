ade0548aaed33fb894d2e49d1b44af874ccd46d8
Logan Welsh <lwelsh@chapman.edu>
2021-10-17 14:52:35 -0700

    Implemented very basic touch controls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public CharacterController2D controller;
     public Animator animator;
 
@@ -14,10 +13,44 @@ public class PlayerMovement : MonoBehaviour
 
     bool jump = false;
 
+    int Touches()
+    {
+        int touchAxis = 0;
+        foreach(Touch touch in Input.touches)
+        {
+            Vector2 touchPos = touch.position - new Vector2(Camera.main.transform.position.x, Camera.main.transform.position.y);
+            float ratio = touchPos.x / (float)Camera.main.scaledPixelWidth;
+            if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary)
+            {
+                if (ratio <= 0.33)
+                {
+                    touchAxis--;
+                }
+                else if (ratio >= 0.66)
+                {
+                    touchAxis++;
+                }
+                else if (touch.phase == TouchPhase.Began)
+                {
+                    jump = true;
+                }
+
+            }
+        }
+
+        return touchAxis;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        Touches();
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") + Touches();
+        if (horizontalMove < -1) horizontalMove = -1;
+        else if (horizontalMove > 1) horizontalMove = 1;
+        horizontalMove *= runSpeed;
+
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
@@ -30,6 +63,7 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        Camera.main.transform.position = new Vector3(controller.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
         jump = false;
     }
 }
