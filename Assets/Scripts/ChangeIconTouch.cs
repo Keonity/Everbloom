f266dc3cd12c233e753fc5b7c78781d2cb62a32b
Keonity <lanoza@chapman.edu>
2021-11-17 00:54:36 -0800

    Updated Merry Meadows.unity, added ChangeIconTouch.cs
    Extended the Merry Meadows level and added "touched" icon functionality.

---
+++ Assets/Scripts/ChangeIconTouch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChangeIconTouch : MonoBehaviour
+{
+
+    public Sprite regSprite;
+    public Sprite downSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) {
+                this.GetComponent<Image>().sprite = downSprite;
+            }
+            else
+            {
+                this.GetComponent<Image>().sprite = regSprite;
+            }
+        }
+    }
+
+    
+}
